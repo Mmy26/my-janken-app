38b241012fdad1b6e0d0f2cc51106aac81c27d2d
Mmy26 <yu.b1994masamura@gmail.com>
2022-11-17 21:22:15 +0900

    Initial Commit

---
+++ my-janken-app/my_janken_appApp.swift
@@ -0,0 +1,18 @@
+//
+//  my_janken_appApp.swift
+//  my-janken-app
+//  
+//  Created by Mmy26 on 2022/11/17.
+//  
+//
+
+import SwiftUI
+
+@main
+struct my_janken_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
